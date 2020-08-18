34da77e09a7d1632216ac48538a359876f2bb8bc
zhurasique <berezovsky.serega@gmail.com>
2020-08-18 23:24:06 +0200

    add route groups

--- routes/api.php
+++ routes/api.php
@@ -18,13 +18,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('categories', 'Api\CategoryController@index');
-Route::get('categories/{id}', 'Api\CategoryController@show')->where('id', '[0-9]+');
-Route::get('categories/paginate', 'Api\CategoryController@paginate')->name('paginate');
+Route::namespace('Api')->prefix('categories')->group(function () {
+    Route::get('/', 'CategoryController@index');
+    Route::get('/{id}', 'CategoryController@show')->where('id', '[0-9]+');
+    Route::get('/platform/{id}', 'CategoryController@getByPlatformId')->where('id', '[0-9]+');
+    Route::get('/paginate', 'CategoryController@paginate')->name('paginate');
+});
 
-Route::get('platforms', 'Api\PlatformController@index');
-Route::get('platforms/{id}', 'Api\PlatformController@show')->where('id', '[0-9]+');
-Route::get('platforms/paginate', 'Api\PlatformController@paginate')->name('paginate');
-Route::post('platforms', 'Api\PlatformController@store');
-Route::delete('platforms/{id}', 'Api\PlatformController@destroy');
-Route::put('platforms/{id}', 'Api\PlatformController@update');
+Route::namespace('Api')->prefix('platforms')->group(function () {
+    Route::get('/', 'PlatformController@index');
+    Route::get('/{id}', 'PlatformController@show')->where('id', '[0-9]+');
+    Route::get('/paginate', 'PlatformController@paginate')->name('paginate');
+    Route::post('/', 'PlatformController@store');
+    Route::delete('/{id}', 'PlatformController@destroy');
+    Route::put('/{id}', 'PlatformController@update');
+});
+
+
+
